ca6189d2f349330ee5a04ee0c39ae93221bad3c2
Roman <g4vr0m@gmail.com>
2023-01-27 15:46:08 +0200

    add success notification on mail sent

--- src/components/RequestStats/index.jsx
+++ src/components/RequestStats/index.jsx
@@ -1,13 +1,17 @@
+import { useState } from 'react';
 import axios from 'axios';
 import { notifyError } from '../../utils/notifications';
-import { StyledContainer, StyledButton } from './styles';
+import { StyledContainer, StyledButton, SuccessWrapper } from './styles';
 
 axios.defaults.baseURL = 'https://nest-demo-server.herokuapp.com';
 
 export const RequestStats = ({ email }) => {
+  const [isSent, setIsSent] = useState(false);
+
   const handleButtonClick = async () => {
     try {
       await axios.post('/mailing/stats', { email });
+      setIsSent(true);
     } catch (error) {
       console.error(error);
       notifyError("Dude, you've broken my app :(");
@@ -16,9 +20,13 @@ export const RequestStats = ({ email }) => {
 
   return (
     <StyledContainer>
-      <StyledButton onClick={handleButtonClick}>
-        Send me an email with BTC stats!
-      </StyledButton>
+      {isSent ? (
+        <SuccessWrapper>Good job</SuccessWrapper>
+      ) : (
+        <StyledButton onClick={handleButtonClick}>
+          Send me an email with BTC stats!
+        </StyledButton>
+      )}
     </StyledContainer>
   );
 };
--- src/components/RequestStats/styles.js
+++ src/components/RequestStats/styles.js
@@ -22,3 +22,18 @@ export const StyledButton = styled.button`
     color: black;
   }
 `;
+
+export const SuccessWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 60px;
+  background: linear-gradient(
+    90deg,
+    rgba(238, 174, 202, 1) 0%,
+    rgba(148, 187, 233, 1) 100%
+  );
+  border: none;
+  border-radius: 10px;
+`;
